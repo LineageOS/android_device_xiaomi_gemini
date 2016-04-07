2cb7249fb0d766f6c5f32aca01697e4e11b79b1b
Kiran Kelageri <kirankelageri@codeaurora.org>
2016-04-07 12:26:54 -0700

    msm8996: Add fs_config support for custom binary.
    Build custom target binary to override the default
    permission.
    
    Change-Id: Ia3533d0859f219597b36b10aaa63f780c66cd218

--- device.mk
+++ device.mk
@@ -163,6 +163,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fingerprintd
 
+# For android_filesystem_config.h
+PRODUCT_PACKAGES += \
+    fs_config_files
+
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8996 \
