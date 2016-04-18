da0d325afc7df39e3f8e53061d9f7a3e0ab36825
Bruno Martins <bgcngm@gmail.com>
2016-04-18 15:08:00 +0100

    gemini: Use prebuilt kernel
    Kernel source code not yet available

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,12 +45,8 @@ BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=16M@0-0xffffffff
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_SOURCE := kernel/xiaomi/gemini
-TARGET_KERNEL_CONFIG := cm_gemini_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/zImage
+ 
 # Platform
 TARGET_BOARD_PLATFORM := msm8996
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno530
