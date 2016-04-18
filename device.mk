077111a40f5c0ea0211dd7d8da5989509f003780
Bruno Martins <bgcngm@gmail.com>
2016-04-18 16:38:30 +0100

    gemini: Add sensors configs

--- device.mk
+++ device.mk
@@ -160,6 +160,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     sensors.msm8996
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf \
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
+
 # WiFi
 PRODUCT_PACKAGES += \
     dhcpcd.conf \
