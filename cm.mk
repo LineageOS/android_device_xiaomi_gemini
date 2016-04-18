e8b120bbaf152601704776246e900b5c212ad3c3
Bruno Martins <bgcngm@gmail.com>
2016-04-18 12:07:13 +0100

    gemini: Add initial device tree files

---
+++ cm.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/xiaomi/gemini/full_gemini.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+PRODUCT_NAME := cm_gemini
+BOARD_VENDOR := Xiaomi
+TARGET_VENDOR := Xiaomi
+PRODUCT_DEVICE := gemini
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="gemini" \
+    PRODUCT_NAME="gemini" \
+    BUILD_FINGERPRINT="Xiaomi/gemini/gemini:6.0/MRA58K/V7.2.12.0.MAACNDB:user/release-keys" \
+    PRIVATE_BUILD_DESC="gemini-user 6.0 MRA58K V7.2.12.0.MAACNDB release-keys"
---
+++ vendorsetup.sh
@@ -0,0 +1,1 @@
+add_lunch_combo cm_gemini-userdebug
