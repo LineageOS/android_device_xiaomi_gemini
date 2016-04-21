b71101d60b0f0aae5f7cb4e6cf9b517a2589bd8c
Louis Popi <theh2o64@gmail.com>
2016-04-21 20:29:44 +0200

    gemini: Switch RIL to CAF variant and enable Protocol Buffers

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -152,6 +152,10 @@ BOARD_USES_QC_TIME_SERVICES := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
+# RIL
+PROTOBUF_SUPPORTED := true
+TARGET_RIL_VARIANT := caf
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
