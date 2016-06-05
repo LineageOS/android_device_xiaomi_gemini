b2793cbddfdf7c74b3a9a57c10e5e2810b1e244f
Bruno Martins <bgcngm@gmail.com>
2016-06-06 00:06:38 +0100

    gemini: Remove unbuilt sensors HAL
    Change-Id: I8e19a613271d601084ae4d398c9ba4dce86e08e4

--- device.mk
+++ device.mk
@@ -261,9 +261,6 @@ PRODUCT_PACKAGES += \
     libxml2
 
 # Sensors
-PRODUCT_PACKAGES += \
-    sensors.msm8996
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
