05c9d04268c0c34ff702ed9c85d4e31f2e2abbfa
Bruno Martins <bgcngm@gmail.com>
2016-06-25 00:50:33 +0100

    gemini: Enable QC location services
    * This is a dependency for DPM on the new blobs
    
    Change-Id: I000ac5722ea7087a6ab1f88ad205542382e3527c

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -81,6 +81,7 @@ PRODUCT_PACKAGES += \\
 
 PRODUCT_PACKAGES += \\
     CNEService \\
+    com.qualcomm.location \\
     dpmserviceapp \\
     qcrilmsgtunnel \\
     QtiTetherService
@@ -219,6 +220,17 @@ LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_SRC_FILES := proprietary/priv-app/CNEService/CNEService.apk
+LOCAL_CERTIFICATE := platform
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := com.qualcomm.location
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_TAGS := optional
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
 LOCAL_CERTIFICATE := platform
 include \$(BUILD_PREBUILT)
 
