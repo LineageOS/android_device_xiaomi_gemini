ef9332942f74bbec62a849b2d033d01e61b66bd7
Steve Kondik <steve@cyngn.com>
2016-07-28 05:06:26 -0700

    gemini: Lights improvements
     * Set up the RGB sync stuff, add happy dance and rainbows too omg!
    
    Change-Id: I3517994c93ccc2d1789d7ceecf8b445ff3d27d5b

--- liblight/lights.c
+++ liblight/lights.c
@@ -112,6 +112,9 @@ static const char GREEN_BLINK_FILE[]
 static const char BLUE_BLINK_FILE[]
         = "/sys/class/leds/blue/blink";
 
+static const char RGB_BLINK_FILE[]
+        = "/sys/class/leds/rgb/rgb_blink";
+
 #define RAMP_SIZE 8
 static int BRIGHTNESS_RAMP[RAMP_SIZE]
         = { 0, 12, 25, 37, 50, 72, 85, 100 };
@@ -259,16 +262,10 @@ static int set_speaker_light_locked(struct light_device_t* dev,
     red = (colorRGB >> 16) & 0xFF;
     green = (colorRGB >> 8) & 0xFF;
     blue = colorRGB & 0xFF;
-    // Bias for true white
-    if (colorRGB != 0 && red == green && green == blue) {
-        blue = (blue * 171) / 256;
-    }
     blink = onMS > 0 && offMS > 0;
 
     // Disable all blinking to start
-    write_int(RED_BLINK_FILE, 0);
-    write_int(GREEN_BLINK_FILE, 0);
-    write_int(BLUE_BLINK_FILE, 0);
+    write_int(RGB_BLINK_FILE, 0);
 
     if (blink) {
         stepDuration = RAMP_STEP_DURATION;
@@ -312,11 +309,14 @@ static int set_speaker_light_locked(struct light_device_t* dev,
         free(duty);
 
         // Start the party
-        write_int(RED_BLINK_FILE, red);
-        write_int(GREEN_BLINK_FILE, green);
-        write_int(BLUE_BLINK_FILE, blue);
+        write_int(RGB_BLINK_FILE, 1);
 
     } else {
+        if (red == 0 && green == 0 && blue == 0) {
+            write_int(RED_BLINK_FILE, 0);
+            write_int(GREEN_BLINK_FILE, 0);
+            write_int(BLUE_BLINK_FILE, 0);
+        }
         write_int(RED_LED_FILE, red);
         write_int(GREEN_LED_FILE, green);
         write_int(BLUE_LED_FILE, blue);
