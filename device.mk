6d720e29b47c733a0db4bafd0716f13e9b06bd0b
Bruno Martins <bgcngm@gmail.com>
2016-08-10 23:03:33 +0100

    gemini: Bring up audio with source built HAL
     * Cleanup mixer paths and set the proper device backends
     * Remove ACDB ID's for sound devices not indexed
     * Remove audio smartPA stuff while at it
    
    Change-Id: I12c96dfff7fb06a66416a5a818843e3945c03a1e

--- device.mk
+++ device.mk
@@ -86,6 +86,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audiod \
     audio.a2dp.default \
+    audio.primary.msm8996 \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
