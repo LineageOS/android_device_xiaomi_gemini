f834e93cfedf329119535ac09136f10845802014
Bruno Martins <bgcngm@gmail.com>
2016-09-10 00:59:15 +0100

    gemini: Use WLAN driver built into kernel
    Change-Id: I6f0a9f03622f4540acd78740221d706003ae2719

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -201,8 +201,6 @@ HOSTAPD_VERSION := VER_0_8_X
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME := "wlan"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
