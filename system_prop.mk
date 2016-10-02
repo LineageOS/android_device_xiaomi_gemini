da47ab39938d1e7fd317942f1627f02f2163d276
Joey Rizzoli <joey@cyanogenmoditalia.it>
2016-10-02 14:58:40 +0200

    gemini: Update sdcardfs prop for N
    Change-Id: I0a837e9761286f415b55df21265fb6387835da06
    Signed-off-by: Joey Rizzoli <joey@cyanogenmoditalia.it>

--- system_prop.mk
+++ system_prop.mk
@@ -178,7 +178,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Storage
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sdcardfs.enable=true
+    ro.sys.sdcardfs=true
 
 # TimeService
 PRODUCT_PROPERTY_OVERRIDES += \
