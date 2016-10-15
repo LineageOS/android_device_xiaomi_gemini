fcd0ba024bfe17276d3e81ba8a0c4c54ba25db02
Bruno Martins <bgcngm@gmail.com>
2016-10-15 22:04:28 +0100

    gemini: Temporarily disable Ambient Display
    Change-Id: I058fe94136e1c2a11317e819813d4979692cd6a0

--- device.mk
+++ device.mk
@@ -148,8 +148,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/calib.cfg:system/etc/calib.cfg
 
 # Doze mode
-PRODUCT_PACKAGES += \
-    XiaomiDoze
+#PRODUCT_PACKAGES += \
+#    XiaomiDoze
 
 # Fingerprint
 PRODUCT_PACKAGES += \
