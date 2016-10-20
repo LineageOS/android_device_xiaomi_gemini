cffe205522847cd6ca6efdad200ac1b2afaea736
Louis Popi <theh2o64@gmail.com>
2016-10-20 19:38:27 +0200

    gemini: Add ConfigPanel for button swap
     * Based on Oppo's implementation
    
    Change-Id: Ia66fd3fed7cc1687862ab1d35578b525a422f0bb

---
+++ configpanel/src/com/cyanogenmod/settings/device/utils/Constants.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cyanogenmod.settings.device.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class Constants {
+
+    // Swap keys
+    public static final String BUTTON_SWAP_KEY = "button_swap";
+
+    // Swap nodes
+    public static final String BUTTON_SWAP_NODE = "/sys/devices/soc/75ba000.i2c/i2c-12/12-0020/input/input1/key_reverse";
+
+    // Holds <preference_key> -> <proc_node> mapping
+    public static final Map<String, String> sBooleanNodePreferenceMap = new HashMap<>();
+    public static final Map<String, String> sStringNodePreferenceMap = new HashMap<>();
+
+    // Holds <preference_key> -> <default_values> mapping
+    public static final Map<String, Object> sNodeDefaultMap = new HashMap<>();
+
+    public static final String[] sButtonPrefKeys = {
+        BUTTON_SWAP_KEY,
+    };
+
+    static {
+        sBooleanNodePreferenceMap.put(BUTTON_SWAP_KEY, BUTTON_SWAP_NODE);
+        sNodeDefaultMap.put(BUTTON_SWAP_KEY, false);
+    }
+
+    public static boolean isPreferenceEnabled(Context context, String key) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getBoolean(key, (Boolean) sNodeDefaultMap.get(key));
+    }
+
+    public static String getPreferenceString(Context context, String key) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getString(key, (String) sNodeDefaultMap.get(key));
+    }
+}
