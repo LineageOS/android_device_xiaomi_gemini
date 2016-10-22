47c70950e3b188bf091f318a54788a000774d2b5
Louis Popi <theh2o64@gmail.com>
2016-10-22 09:47:49 +0200

    gemini: Add fingerprint as home button
     * No longer allow home button to wake device
    
    Change-Id: Ib6a5d994798d8d24d23ccfbdefaeadaf2d3fe8cf

--- configpanel/src/com/cyanogenmod/settings/device/Startup.java
+++ configpanel/src/com/cyanogenmod/settings/device/Startup.java
@@ -89,7 +89,8 @@ public class Startup extends BroadcastReceiver {
     }
 
     static boolean hasButtonProcs() {
-        return new File(Constants.BUTTON_SWAP_NODE).exists();
+        return (new File(Constants.BUTTON_SWAP_NODE).exists() ||
+            new File(Constants.FP_HOME_NODE).exists());
     }
 
     private void disableComponent(Context context, String component) {
