a40aa7dcdbb7b26a1fb7eba9f304d21b8ecf133c
Bruno Martins <bgcngm@gmail.com>
2016-11-16 03:04:50 -0700

    Revert "gemini: Enable sdcardfs"
    Still causing issues on certain applications, not being able to write
    to the internal sdcard.
    
    This reverts commit cda97deeb4c64b8f2d14c13217a9ea8bded594d8.
    
    Change-Id: I9b7c124fae0938f33d646a6195d73373366a7a2c

--- system_prop.mk
+++ system_prop.mk
@@ -165,10 +165,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.df.iwlan_mux=9 \
     persist.data.df.dev_name=rmnet_usb0
 
-# Storage
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sdcardfs.enable=true
-
 # TimeService
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
