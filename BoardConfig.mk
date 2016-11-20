6a309b371dc76042a264d4da97d450b4e7982211
Bruno Martins <bgcngm@gmail.com>
2016-11-20 22:43:01 +0000

    gemini: Build LiveDisplay native interface library
    Change-Id: I1dfa7c7a349811f9a3b0e628aebbf26093e1cd79

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,6 +115,7 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_HARDWARE_CLASS += \
     hardware/cyanogen/cmhw \
     $(DEVICE_PATH)/cmhw
+BOARD_USES_CYANOGEN_HARDWARE := true
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # CNE and DPM
