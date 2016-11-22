96e6cba61f7d2f85e0d094ce49630fe79658c4d5
Bruno Martins <bgcngm@gmail.com>
2016-11-22 04:19:53 -0800

    gemini: Update USB configs
     * Add changes from latest stock configs
     * Do some additional clean ups
     * Include diag mode (proven to be useful to those who want to play
       with QPST and QXDM for changing modem configs)
    
    Change-Id: Ie84b4bf907003aa9fc9c4bf5e8618bcc26a206b8

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -52,6 +52,14 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.usb.diag.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.qcom.usb.diag.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.target.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
