9a0805b7b5c0fd6fd1eac76c3a14764a511ff985
Bruno Martins <bgcngm@gmail.com>
2016-11-23 06:49:54 -0800

    gemini: Merge misc 6.11.23 (N) stock configs
    Change-Id: I58b51f3caeb33c1a54652de31e0ea435b275118b

--- system_prop.mk
+++ system_prop.mk
@@ -39,6 +39,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.gyro.disable=0 \
     persist.camera.imglib.fddsp=1
 
 # CNE
@@ -142,7 +143,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.qmi.adb_logmask=0 \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
-    persist.logd.size.radio=4M \
     persist.net.doxlat=true \
     persist.radio.apm_sim_not_pwdn=1 \
     persist.radio.force_on_dc=true \
@@ -173,6 +173,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # TimeService
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
+
+# USB
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.usb.controller=6a00000.dwc3
 
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
