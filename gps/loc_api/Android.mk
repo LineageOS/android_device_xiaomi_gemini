be0ccf36945a571f1f1945fd8d9d0fed3f682430
Bruno Martins <bgcngm@gmail.com>
2016-12-04 12:06:39 +0000

    gemini: Import QC location source
     * Tag: LA.UM.5.5.r1-01800-8x96.0
    
    Change-Id: Ib33b01591c1743dd8b217a81a580bc3c2b13bd05

--- gps/loc_api/Android.mk
+++ gps/loc_api/Android.mk
@@ -9,5 +9,11 @@ endif #TARGET_NO_RPC
 
 GPS_DIR_LIST += $(LOCAL_PATH)/libloc_api_50001/
 
+ifneq ($(QCPATH),)
+GPS_DIR_LIST += \
+    $(LOCAL_PATH)/ds_api/ \
+    $(LOCAL_PATH)/loc_api_v02/
+endif
+
 #call the subfolders
 include $(addsuffix Android.mk, $(GPS_DIR_LIST))
