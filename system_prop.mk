19bd6a29945deb8d3d60d8dc268546aae64f0fe5
umang96 <umangleekha3@gmail.com>
2016-12-12 12:45:58 +0530

    gemini: Fix video recording breaks
     * Video recording used to work fine when device boots,
       but if we try recording video after a deep sleep session,
       then videos recorded are frozen. This will fix the issue.
    
    Change-Id: Icfe490252dede871852ba305276bf262ea94fbfe

--- system_prop.mk
+++ system_prop.mk
@@ -49,6 +49,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
+    media.camera.ts.monotonic=0 \
     persist.camera.gyro.disable=0 \
     persist.camera.imglib.fddsp=1
 
