bd732abec501da13390322b02db1d85c1b5ac722
Bruno Martins <bgcngm@gmail.com>
2016-12-14 10:47:49 +0000

    gemini: Support fingerprint as home button
     * Take advantage of the fact that Xiaomi's fingerprint HAL
       now sends KEY_KPENTER (keycode 96) and interpret it as
       KEYCODE_HOME (keycode 102) - a home key event
     * The ability to wake up device using home button can also be enabled
       again because the fingerprint HAL sends key 96 only when
       device is turned on and unlocked
    
    Change-Id: I29782b73a62724e5b706d6edf366d180893f2ce9

--- configpanel/src/com/cyanogenmod/settings/device/utils/Constants.java
+++ configpanel/src/com/cyanogenmod/settings/device/utils/Constants.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
+ *           (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,16 +23,20 @@ import java.util.Map;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.preference.SwitchPreference;
 
 public class Constants {
 
     // Preference keys
     public static final String BUTTON_SWAP_KEY = "button_swap";
+    public static final String FP_HOME_KEY = "fp_home";
     public static final String FP_WAKEUP_KEY = "fp_wakeup";
 
     // Nodes
     public static final String BUTTON_SWAP_NODE = "/proc/touchpanel/reversed_keys_enable";
+    public static final String FP_HOME_KEY_NODE = "/sys/devices/soc/soc:fpc_fpc1020/enable_key_events";
     public static final String FP_WAKEUP_NODE = "/sys/devices/soc/soc:fpc_fpc1020/enable_wakeup";
+    public static final String VIRTUAL_KEYS_NODE = "/proc/touchpanel/capacitive_keys_enable";
 
     // Holds <preference_key> -> <proc_node> mapping
     public static final Map<String, String> sBooleanNodePreferenceMap = new HashMap<>();
@@ -40,17 +45,28 @@ public class Constants {
     // Holds <preference_key> -> <default_values> mapping
     public static final Map<String, Object> sNodeDefaultMap = new HashMap<>();
 
+    // Holds <preference_key> -> <user_set_values> mapping
+    public static final Map<String, Object[]> sNodeUserSetValuesMap = new HashMap<>();
+
+    // Holds <preference_key> -> <dependency_check> mapping
+    public static final Map<String, String[]> sNodeDependencyMap = new HashMap<>();
+
     public static final String[] sButtonPrefKeys = {
         BUTTON_SWAP_KEY,
+        FP_HOME_KEY,
         FP_WAKEUP_KEY,
     };
 
     static {
         sBooleanNodePreferenceMap.put(BUTTON_SWAP_KEY, BUTTON_SWAP_NODE);
+        sBooleanNodePreferenceMap.put(FP_HOME_KEY, FP_HOME_KEY_NODE);
         sBooleanNodePreferenceMap.put(FP_WAKEUP_KEY, FP_WAKEUP_NODE);
 
         sNodeDefaultMap.put(BUTTON_SWAP_KEY, false);
+        sNodeDefaultMap.put(FP_HOME_KEY, false);
         sNodeDefaultMap.put(FP_WAKEUP_KEY, true);
+
+        sNodeDependencyMap.put(FP_HOME_KEY, new String[]{ VIRTUAL_KEYS_NODE, "1" });
     }
 
     public static boolean isPreferenceEnabled(Context context, String key) {
@@ -61,5 +77,25 @@ public class Constants {
     public static String getPreferenceString(Context context, String key) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
         return preferences.getString(key, (String) sNodeDefaultMap.get(key));
+    }
+
+    public static void updateDependentPreference(Context context, SwitchPreference b,
+            String key, Boolean shouldSetEnabled) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean prefActualValue = preferences.getBoolean(key, false);
+
+        if (shouldSetEnabled) {
+            if (sNodeUserSetValuesMap.get(key) != null &&
+                    (Boolean) sNodeUserSetValuesMap.get(key)[1] &&
+                    (Boolean) sNodeUserSetValuesMap.get(key)[1] != prefActualValue) {
+                b.setChecked(true);
+                sNodeUserSetValuesMap.put(key, new Boolean[]{ prefActualValue, false });
+            }
+        } else {
+            if (b.isEnabled() && prefActualValue)
+                sNodeUserSetValuesMap.put(key, new Boolean[]{ prefActualValue, true });
+            b.setEnabled(false);
+            b.setChecked(false);
+        }
     }
 }
