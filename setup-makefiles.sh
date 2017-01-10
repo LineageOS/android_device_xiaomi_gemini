c872a0bba9ef5ede0ea696653de9a0d4d74cc9a2
Bruno Martins <bgcngm@gmail.com>
2017-01-10 22:17:02 +0000

    gemini: Set device bring up year
    Change-Id: I2aa73e6f91fbf08e20873605fcc6e1c3214c2204

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,6 +20,8 @@ set -e
 
 DEVICE=gemini
 VENDOR=xiaomi
+
+INITIAL_COPYRIGHT_YEAR=2016
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
