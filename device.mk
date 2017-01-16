281e4eed42d13a014981748298af4382183f4eea
Freddi <muff99@outlook.com>
2017-01-16 21:54:47 +0100

    readd su
    because of https://review.lineageos.org/#/c/154019/

--- device.mk
+++ device.mk
@@ -81,6 +81,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
+# su
+WITH_SU := true
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
