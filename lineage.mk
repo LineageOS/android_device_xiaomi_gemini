16739891c2fa7c498be8025022ecc78d311f3ee0
Bruno Martins <bgcngm@gmail.com>
2017-01-20 14:02:49 +0000

    gemini: Merge MIUI 7.1.19 global dev configs
    Change-Id: I971b5c9a422305585e8fae97ea7a7b617e68dada

--- lineage.mk
+++ lineage.mk
@@ -37,7 +37,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="gemini" \
     PRODUCT_NAME="gemini" \
-    BUILD_FINGERPRINT="Xiaomi/gemini/gemini:7.0/NRD90M/6.12.22:user/release-keys" \
-    PRIVATE_BUILD_DESC="gemini-user 7.0 NRD90M 6.12.22 release-keys"
+    BUILD_FINGERPRINT="Xiaomi/gemini/gemini:7.0/NRD90M/7.1.19:user/release-keys" \
+    PRIVATE_BUILD_DESC="gemini-user 7.0 NRD90M 7.1.19 release-keys"
 
 TARGET_VENDOR := Xiaomi
