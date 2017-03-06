8927ae88fa8b4e0b7f8f38e0eeb4add9bbead838
Bruno Martins <bgcngm@gmail.com>
2017-03-06 11:28:31 +0000

    gemini: Use extractors from msm8996-common
     * Ensure that device bring up year is set so it can be
       inherited in msm8996-common
    
    Change-Id: I9e110735f36f85cc78250d483f5ff9a433a27197

--- extract-files.sh
+++ extract-files.sh
@@ -18,41 +18,11 @@
 
 set -e
 
-DEVICE=gemini
-VENDOR=xiaomi
+# Required!
+export DEVICE=gemini
+export DEVICE_COMMON=msm8996-common
+export VENDOR=xiaomi
 
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+export DEVICE_BRINGUP_YEAR=2016
 
-CM_ROOT="$MY_DIR"/../../..
-
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
-    exit 1
-fi
-. "$HELPER"
-
-if [ $# -eq 0 ]; then
-    SRC=adb
-else
-    if [ $# -eq 1 ]; then
-        SRC=$1
-    else
-        echo "$0: bad number of arguments"
-        echo ""
-        echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-        echo ""
-        echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-        echo "the device using adb pull."
-        exit 1
-    fi
-fi
-
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
-
-extract "$MY_DIR"/proprietary-files.txt "$SRC"
-
-"$MY_DIR"/setup-makefiles.sh
+./../../$VENDOR/$DEVICE_COMMON/extract-files.sh $@
