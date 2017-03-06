ad78fe4be4128d4731b772c6d3694a65fcbf542a
Bruno Martins <bgcngm@gmail.com>
2017-03-06 11:21:46 +0000

    gemini: Inherit from msm8996-common
    Change-Id: Ic35111366a61163d5db92fb5d45ef9a06a883e4d

--- recovery/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := bootable/recovery
-LOCAL_SRC_FILES := recovery_updater.cpp
-LOCAL_MODULE := librecovery_updater_gemini
-LOCAL_MODULE_TAGS := eng
-include $(BUILD_STATIC_LIBRARY)
