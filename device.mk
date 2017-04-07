70821cf0508c533618683cd215aaa82ba851c7ac
Glen Kuhne <kuh@google.com>
2017-04-07 14:54:17 -0700

    gemini: Set default hotspot ssid
    Added a device specific overlay that changes the default name
    from "AndroidAp" to customized name
    
    Test: Builds, manually verified name change
    Bug: 36368950
    Change-Id: I38a5f6fb950d6a45342080bae186a8ed276d94bf

--- device.mk
+++ device.mk
@@ -80,3 +80,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8996-lite.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996-lite.conf \
     $(LOCAL_PATH)/configs/thermal-engine-8996.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996.conf
+
+# Wifi
+PRODUCT_PACKAGES += \
+    TargetWifiOverlay
