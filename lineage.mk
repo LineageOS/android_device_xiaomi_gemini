eab58fd8948fc6899cf0cebaa2f5b9d708945f7a
Bruno Martins <bgcngm@gmail.com>
2017-05-30 16:00:35 +0000

    gemini: Update to the latest build fingerprint
     * From Global Stable V8.5.2.0.NAACNED (MIUI8)
    
    Change-Id: Ie0d06f877af417c3204a6ba8bd72650983baa2b8

--- lineage.mk
+++ lineage.mk
@@ -37,7 +37,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="gemini" \
     PRODUCT_NAME="gemini" \
-    BUILD_FINGERPRINT="Xiaomi/gemini/gemini:7.0/NRD90M/V8.2.1.0.NAACNEB:user/release-keys" \
-    PRIVATE_BUILD_DESC="gemini-user 7.0 NRD90M V8.2.1.0.NAACNEB release-keys"
+    BUILD_FINGERPRINT="Xiaomi/gemini/gemini:7.0/NRD90M/V8.5.2.0.NAACNED:user/release-keys" \
+    PRIVATE_BUILD_DESC="gemini-user 7.0 NRD90M V8.5.2.0.NAACNED release-keys"
 
 TARGET_VENDOR := Xiaomi
