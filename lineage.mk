c28dd625e95fc8850d69b211c76a3a9b2c5a9fd7
Luca Stefani <luca020400@lineageos.org>
2017-08-19 13:17:46 +0200

    gemini: Inherit from Lineage vendor
    Change-Id: I6d60ee5643181781ba8b386f1f16cfb59610f35c

--- lineage.mk
+++ lineage.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/gemini/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_gemini
