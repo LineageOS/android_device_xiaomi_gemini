4e68514c0c53e55cbcb138063ee0c2e203797185
Bruno Martins <bgcngm@gmail.com>
2017-08-24 12:07:42 +0100

    [DNM] gemini: O bringup
     * Disable breaking packages
     * Remove breaking overlays
    
    Change-Id: I081a04420baea86c380899ad130fd53ef31b25e2

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ $(call inherit-product, vendor/xiaomi/gemini/gemini-vendor.mk)
 # Local overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+#    $(LOCAL_PATH)/overlay-lineage
 
 # Inherit from msm8996-common
 $(call inherit-product, device/xiaomi/msm8996-common/msm8996.mk)
@@ -36,8 +36,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:system/etc/mixer_paths_tasha.xml
 
 # ConfigPanel
-PRODUCT_PACKAGES += \
-    ConfigPanel
+#PRODUCT_PACKAGES += \
+#    ConfigPanel
 
 # Consumerir
 PRODUCT_PACKAGES += \
