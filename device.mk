e782756dfbe60434e3bd4a676669921b9ebcdd3c
Andre Eisenbach <eisenbach@google.com>
2017-12-06 16:16:36 -0800

    gemini: Rename libnfc-brcm.conf -> libnfc-nci.conf
    Test: manual
    Merged-In: I9655a14e021024540cf342022cb817e1f8d0f9ad
    Change-Id: I9655a14e021024540cf342022cb817e1f8d0f9ad

--- device.mk
+++ device.mk
@@ -69,7 +69,7 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # TFA calibration
