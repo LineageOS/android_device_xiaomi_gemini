ac62e94977746e99a5906b7521a02aacc810fa42
Bruno Martins <bgcngm@gmail.com>
2017-12-16 22:09:09 +0000

    gemini: Add compatibility matrix
    Change-Id: Ibf6053f1a7e9df5384664379772cb8757e897622

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,7 @@ TARGET_BOARD_INFO_FILE ?= $(DEVICE_PATH)/board-info.txt
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Kernel
 TARGET_KERNEL_CONFIG := gemini_defconfig
