0083e086886c1a8eb7a48b19605c5631206d1bc6
Bruno Martins <bgcngm@gmail.com>
2017-12-22 23:45:34 +0000

    gemini: Move device-specific feature configs to vendor
    Change-Id: I856965935d3308c33e0e19c49451df968ab20304

--- device.mk
+++ device.mk
@@ -28,7 +28,7 @@ $(call inherit-product, device/xiaomi/msm8996-common/msm8996.mk)
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
 # Audio
 PRODUCT_COPY_FILES += \
