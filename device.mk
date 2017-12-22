fd3c767771491d59d758c25aaa3c16323705fc66
Bruno Martins <bgcngm@gmail.com>
2017-12-22 23:44:33 +0000

    gemini: Move NFC configs to vendor
    Change-Id: I7ae2cc544e79fd22cc4523b79f545e9210dd64d1

--- device.mk
+++ device.mk
@@ -63,5 +63,5 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
+    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
