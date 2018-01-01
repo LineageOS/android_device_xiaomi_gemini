e7a45b68b1c1833f6ce444eb3b70eda2f7fa1bb8
LuK1337 <priv.luk@gmail.com>
2018-01-01 23:10:58 +0100

    gemini: Implement on boot TFA98xx calibration
    * TFA amplifier requires to play a calibration
      sound file on each boot otherwise it calibrates
      on the first played sound eg: unlock phone sound.
    
    Change-Id: Ice91c29d14e30ed688007cffff56375b178106c2

--- device.mk
+++ device.mk
@@ -23,6 +23,11 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# TFA calibration
+PRODUCT_PACKAGES += \
+    init.tfa.sh \
+    tinyplay
+
 # Inherit from msm8996-common
 $(call inherit-product, device/xiaomi/msm8996-common/msm8996.mk)
 
