28f1f003d5d28312ae5f1818dcc82671c929063a
LuK1337 <priv.luk@gmail.com>
2018-01-20 16:38:45 +0100

    gemini: Move init.target.rc to /vendor
    Change-Id: I41cd9901d4301fb01b5166d778ed73f5c6a477de

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -17,5 +17,5 @@ LOCAL_MODULE       := init.target.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.target.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
