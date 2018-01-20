aa772892afe2d05305bb6fe8242cf7a3e340b29a
LuK1337 <priv.luk@gmail.com>
2018-01-20 16:38:45 +0100

    gemini: Move init.tfa.sh to /vendor
    Change-Id: I6c4e78e9a1ad486e03bcb0e14f9bdbb216b697e5

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE       := init.tfa.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.tfa.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 # Device init scripts
