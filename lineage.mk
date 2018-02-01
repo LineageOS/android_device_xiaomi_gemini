502595e3dfb22f26836b65731b005b6cc0b26030
Bruno Martins <bgcngm@gmail.com>
2018-02-01 22:08:01 +0100

    gemini: Set matching build fingerprint for vendor.img
    Change-Id: I555c15eddc6dff694285ab1b23f6f9adfd64e535

--- lineage.mk
+++ lineage.mk
@@ -37,7 +37,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="gemini" \
     PRODUCT_NAME="gemini" \
-    BUILD_FINGERPRINT="Xiaomi/gemini/gemini:7.0/NRD90M/V8.2.1.0.NAACNEB:user/release-keys" \
     PRIVATE_BUILD_DESC="gemini-user 7.0 NRD90M V8.2.1.0.NAACNEB release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Xiaomi/gemini/gemini:7.0/NRD90M/V8.2.1.0.NAACNEB:user/release-keys"
 
 TARGET_VENDOR := Xiaomi
