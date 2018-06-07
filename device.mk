97d83d6eb1e90ffbb02e653470db3df07b19b995
Giuseppe Barillari <joe2k01dev@gmail.com>
2018-06-07 16:37:19 +0200

    Revert "gemini: Build Snap"
    This reverts commit b35e3576796a479ca893aa4ee03ded61dd23b1ec.
    Moved back to common tree.
    
    Change-Id: If30d0af0c4a74094691a6b621a0925bd9b32ec32
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- device.mk
+++ device.mk
@@ -35,10 +35,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml
 
-# Camera
-PRODUCT_PACKAGES += \
-    Snap
-
 # ConfigPanel
 PRODUCT_PACKAGES += \
     ConfigPanel
