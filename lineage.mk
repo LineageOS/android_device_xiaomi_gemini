7998bccf6579b33d2734f0c6761b10cf3f5946c2
Bruno Martins <bgcngm@gmail.com>
2018-07-10 18:48:26 +0100

    gemini: Update build fingerprint from Oreo-based MIUI 9 Global Stable
     * Newly released, just few days after the last build fingerprint update
    
    Change-Id: I34df26424ac952ddaa891375df69113adf60f108

--- lineage.mk
+++ lineage.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -37,9 +37,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="gemini" \
     PRODUCT_NAME="gemini" \
-    PRIVATE_BUILD_DESC="gemini-user 7.0 NRD90M V9.5.5.0.NAAMIFA release-keys"
+    PRIVATE_BUILD_DESC="gemini-user 8.0.0 OPR1.170623.032 V9.6.1.0.OAAMIFD release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/gemini/gemini:7.0/NRD90M/V9.5.5.0.NAAMIFA:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/gemini/gemini:8.0.0/OPR1.170623.032/V9.6.1.0.OAAMIFD:user/release-keys"
 
 TARGET_VENDOR := Xiaomi
