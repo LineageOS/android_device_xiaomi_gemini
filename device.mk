144df8199d2aaace075a9b216f7c90bfa48890bc
Anton Hansson <hansson@google.com>
2018-10-22 15:33:51 +0100

    gemini: Move libnfc-nci.conf to /product
    Device-specific files belong off /system.
    
    Bug: 117878616
    Test: make
    Change-Id: Iddad62c178c553e4d628deb4509ea54451b8ab4f

--- device.mk
+++ device.mk
@@ -72,7 +72,7 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # TFA calibration
