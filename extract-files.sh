66947faa49a8fe6eb32cb2d9b64582bb332790b1
Vladimir Oltean <olteanv@gmail.com>
2019-03-22 22:45:03 +0000

    gemini: Update for latest common extractors
    Change-Id: I4a8f81b2eb05dafc2b022daddb210eb3100e36f0

--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,6 +16,12 @@
 # limitations under the License.
 #
 
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
 set -e
 
 # Required!
@@ -25,4 +31,4 @@ export VENDOR=xiaomi
 
 export DEVICE_BRINGUP_YEAR=2016
 
-./../../$VENDOR/$DEVICE_COMMON/extract-files.sh $@
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
