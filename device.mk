1bd0531b24467e04185bfe3d0a7992f1a0b5d19d
Bruno Martins <bgcngm@gmail.com>
2020-04-08 13:47:28 +0100

    gemini: Move to commonized IR HAL
    The HAL has been moved to hardware/xiaomi and is no longer
    used only for msm8996 targets.
    
    Change-Id: I6948458421bf555282a090de2520988bea208851

--- device.mk
+++ device.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -51,7 +51,7 @@ PRODUCT_PACKAGES += \
 
 # Consumer IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-service.xiaomi_8996
+    android.hardware.ir@1.0-service.xiaomi
 
 # Device init scripts
 PRODUCT_PACKAGES += \
