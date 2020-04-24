85e103e88fbc4986e22c6707984d4a471f91ba23
iKeramat <ikeramat80@gmail.com>
2020-04-24 18:29:49 +0200

    Revert "gemini: Update to the latest and final build fingerprint"
    This reverts commit 45af077d2b11928c012c8264b8b7ecfe420a005a.
    
    Reason for revert: Broken ctsProfile in Safetynet test
    
    Change-Id: Ibdf60d34136e2c01ebd80e9d9252ec03a67082bd

--- lineage_gemini.mk
+++ lineage_gemini.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -36,9 +36,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="gemini" \
-    PRIVATE_BUILD_DESC="gemini-user 8.0.0 OPR1.170623.032 V10.2.2.0.OAAMIXM release-keys"
+    PRIVATE_BUILD_DESC="gemini-user 8.0.0 OPR1.170623.032 V9.6.1.0.OAAMIFD release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/gemini/gemini:8.0.0/OPR1.170623.032/V10.2.2.0.OAAMIXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/gemini/gemini:8.0.0/OPR1.170623.032/V9.6.1.0.OAAMIFD:user/release-keys"
 
 TARGET_VENDOR := Xiaomi
