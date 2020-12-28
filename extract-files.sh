e422f25a92f3f733694bb168342f1adbde3065e7
Bruno Martins <bgcngm@gmail.com>
2020-12-28 10:34:04 +0000

    gemini: Align with extraction-utils templates
    Change-Id: Ifbf18f79a1d0f173cb5b83bdf0178db2a5f1373e

--- extract-files.sh
+++ extract-files.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 # If we're being sourced by the common script that we called,
@@ -24,11 +14,8 @@ fi
 
 set -e
 
-# Required!
 export DEVICE=gemini
 export DEVICE_COMMON=msm8996-common
 export VENDOR=xiaomi
-
-export DEVICE_BRINGUP_YEAR=2016
 
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
