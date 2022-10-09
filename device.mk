920e6ddb36edcf469f5417850e9a1cf63be6ba53
Bruno Martins <bgcngm@gmail.com>
2022-10-09 13:43:25 +0100

    gemini: Migrate from init.tfa.sh to tfa-calib
    Change-Id: Ibb99f29c7afac5b080206bada06e8d282e46c335

--- device.mk
+++ device.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2017-2020,2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -70,8 +70,7 @@ PRODUCT_COPY_FILES += \
 
 # TFA calibration
 PRODUCT_PACKAGES += \
-    init.tfa.sh \
-    tinyplay
+    tfa-calib
 
 # Touch HAL
 PRODUCT_PACKAGES += \
