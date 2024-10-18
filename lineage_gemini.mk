7a4d009294587d9bde3ea19ff62cf65d3db46e97
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    gemini: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_gemini.mk
+++ lineage_gemini.mk
@@ -31,10 +31,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="gemini" \
-    PRIVATE_BUILD_DESC="gemini-user 8.0.0 OPR1.170623.032 V9.6.1.0.OAAMIFD release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/gemini/gemini:8.0.0/OPR1.170623.032/V9.6.1.0.OAAMIFD:user/release-keys"
+    BuildDesc="gemini-user 8.0.0 OPR1.170623.032 V9.6.1.0.OAAMIFD release-keys" \
+    BuildFingerprint=Xiaomi/gemini/gemini:8.0.0/OPR1.170623.032/V9.6.1.0.OAAMIFD:user/release-keys
 
 TARGET_VENDOR := Xiaomi
